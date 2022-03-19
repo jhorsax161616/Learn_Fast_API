5066946f0b7baa831b94a09dc79ee4a42ec92fd9
Sax16 <jhorsax161616@gmail.com>
2022-03-19 17:17:18 -0500

    Validations_query

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Python
 from typing import Dict
 from typing import Optional
+from fastapi.param_functions import Query
 
 #Pydantic
 from pydantic import BaseModel
@@ -8,6 +9,7 @@ from pydantic import BaseModel
 #FastAPI
 from fastapi import FastAPI
 from fastapi import Body
+from fastapi import Query
 
 
 app: FastAPI = FastAPI()
@@ -32,3 +34,12 @@ def home() -> Dict:
 @app.post("/person/new")
 def create_person(person: Person = Body(...)): # Body(...) los 3 puntos son para asignar que el parámetro es obligatorio
     return person
+
+#Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=2, max_length=50), #Los query parameters son opcionales y usammos min y max para restringir la entrada
+    age: str = Query(...)
+):
+    return {name: age}
